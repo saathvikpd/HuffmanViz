0dee47a1310e23c3ca37ddd62ee8bed003859a4e
saathvikpd <saathvikpd@users.noreply.github.com>
2024-03-13 12:08:16 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-    import App from '../components/App.svelte'
+    import ScrollyTeller from '../components/ScrollyTeller.svelte'
 </script>
 
 <main>
-    <App />
+    <ScrollyTeller />
 </main>
