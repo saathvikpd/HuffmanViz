8aba55e2f3f43f5672b07d07b701a534c4d9f627
Kai Breese <kai.breeze@me.com>
2024-03-14 01:26:48 -0700

    new svelte project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,2 @@
-<script>
-    import ScrollyTeller from '../components/ScrollyTeller.svelte'
-</script>
-
-<main>
-    <ScrollyTeller />
-</main>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
