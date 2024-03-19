ac273facac8be97a4af3b962f26ceb079e51c003
Kai Breese <kai.breeze@me.com>
2024-03-19 10:33:30 -0700

    styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<script>
+    import '/src/lib/bootstrap/css/bootstrap.min.css/'
+    import '/src/lib/bootstrap/js/bootstrap.bundle.min.js/'
+    export const prerender = true
+    export const trailingSlash = 'always';
+</script>
