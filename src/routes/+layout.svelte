ef772af4bc8d536f4c8590680e11a46c106738f5
saathvikpd <saathvikpd@users.noreply.github.com>
2024-03-19 15:29:31 -0700

    Delete src/routes/+layout.svelte

--- src/routes/+layout.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-    import '/src/lib/bootstrap/css/bootstrap.min.css/'
-    import '/src/lib/bootstrap/js/bootstrap.bundle.min.js/'
-    export const prerender = true
-    export const trailingSlash = 'always';
-</script>
